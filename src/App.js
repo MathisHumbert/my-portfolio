418236db338ad574fdc1a422a13192da53cd4124
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-13 19:05:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import About from './sections/About';
+import Footer from './sections/Footer';
+import Navbar from './sections/Navbar';
+import Projects from './sections/Projects';
+import Skills from './sections/Skills';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/sections/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return <nav>navbar</nav>;
+};
+
+const Wrapper = styled.nav``;
+
+export default Navbar;
