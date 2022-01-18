d422e2ff504f470062cb7e3c56e904523878af03
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-18 09:13:52 +0100

    working on projects page

--- src/sections/Projects/SingleVerProject.jsx
+++ src/sections/Projects/SingleVerProject.jsx
@@ -1,4 +1,7 @@
+import { useRef, useEffect } from 'react';
+import useOnScreen from '../../utils/useOnScreen';
 import styled from 'styled-components';
+import { FaLink, FaGithub } from 'react-icons/fa';
 
 const SingleVerProject = ({
   id,
@@ -11,39 +14,53 @@ const SingleVerProject = ({
   code,
   website,
 }) => {
+  const ref = useRef(null);
+
+  const onScreen = useOnScreen(ref, 0.5);
+
+  useEffect(() => {
+    if (onScreen) {
+      handleCounter(id);
+    }
+  }, [onScreen, id]);
+
   return (
-    <Wrapper key={id} className='single-project-ver'>
-      <div></div>
-      <div className='project-item'>
-        <div className='info'>
-          <h1 className='info-title'>{title}</h1>
-          <h2 className='info-subtitle'>{subtitle}</h2>
-          <p className='info-category'>{category}</p>
-        </div>
-        <div className='image' style={{ backgroundImage: `url(${src})` }}></div>
+    <Wrapper
+      className={
+        onScreen ? 'single-project-ver is-reveal' : 'single-project-ver'
+      }
+      ref={ref}
+    >
+      <div className='info'>
+        <h1 className='info-title'>{title}</h1>
+        <h2 className='info-subtitle'>{subtitle}</h2>
+        <p className='info-tech'>{tech}</p>
+        <p className='info-category'>{category}</p>
       </div>
-      <div></div>
+      <div className='image' style={{ backgroundImage: `url(${src})` }}></div>
+      <div className='link'>
+        <a href={website} className='website-link'>
+          <FaLink className='link-icon' />
+          Voir le résultat du projet
+        </a>
+        <a href={code} className='code-link'>
+          <FaGithub className='link-icon' />
+          Voir le code du projet
+        </a>
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.article`
   width: 100%;
-  min-height: 100vh;
   height: 100%;
-  display: grid;
-  grid-template-rows: 100px 1fr 20px;
-  position: relative;
-
-  .project-item {
-    width: 100%;
-    height: 100%;
-  }
+  padding-top: 50px;
+  padding-bottom: 50px;
 
   .info {
     z-index: 1;
-    color: #dbd8d6;
-    position: absolute;
+    color: var(--grey-color);
 
     &-title {
       font-size: 6vw;
@@ -58,24 +75,70 @@ const Wrapper = styled.article`
       color: transparent;
       font-family: var(--font-bodo);
       -webkit-font-smoothing: antialiased;
-      -webkit-text-stroke: 2px #dbd8d6;
+      -webkit-text-stroke: 2px var(--grey-color);
     }
 
-    &-category {
-      font-size: 24px;
-      line-height: 24px;
+    &-category,
+    &-tech {
+      font-size: 4vw;
+      line-height: 4vw;
       font-family: var(--font-bai);
       font-weight: 400px;
       margin-top: 2vh;
     }
+
+    &-category {
+      margin-top: 10px;
+    }
+  }
+
+  .link {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 10px;
+  }
+
+  .link-icon {
+    font-size: 20px;
   }
 
   .image {
-    height: 100%;
+    aspect-ratio: 2 / 3;
+    height: 50vh;
+    width: 100%;
     background-size: cover;
     background-position: center;
     transform-origin: center;
-    border: 1px solid red;
+    margin: 48px 0;
+    transform: scale(0.7);
+    transition: all 1.5s cubic-bezier(0.77, 0, 0.175, 1);
+    filter: grayscale(100%) sepia(20%) brightness(80%);
+  }
+
+  &.is-reveal {
+    .image {
+      transform: scale(1);
+      filter: none;
+    }
+
+    .project-link {
+      transform: rotate(-90deg) scale(1);
+      filter: none;
+    }
+  }
+
+  .website-link,
+  .code-link {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    color: var(--grey-color);
+    text-decoration: none;
+    font-size: 4vw;
+    line-height: 4vw;
+    font-family: var(--font-bai);
+    font-weight: 400px;
   }
 
   @media (min-width: 768px) {
--- src/sections/Projects/VerProjects.jsx
+++ src/sections/Projects/VerProjects.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { projectsData } from '../../utils/data';
 import SingleVerProject from './SingleVerProject';
 
 const VerProjects = () => {
+  const [counter, setCounter] = useState(1);
+
+  const handleCounter = (id) => setCounter(id);
+
   return (
     <Wrapper className='vertical-container'>
       {projectsData.map((project) => {
-        return <SingleVerProject key={project.id} {...project} />;
+        return (
+          <SingleVerProject
+            key={project.id}
+            {...project}
+            handleCounter={handleCounter}
+          />
+        );
       })}
     </Wrapper>
   );
