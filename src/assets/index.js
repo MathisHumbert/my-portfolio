48022cfe3956a419c53d021c326daf726dd12168
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-24 16:57:32 +0100

    changed project-2 img

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 const images = {
   project1: require('./project-1.jpg'),
-  project2: require('./project-2.png'),
+  project2: require('./project-2.jpg'),
   project3: require('./project-3.jpg'),
 };
 export default images;
