d5ec5c01955b7053756b81920b358de703fb7d79
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-25 09:54:13 +0100

    rect-router-dom set up

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import { useState, useRef, useEffect } from 'react';
-import About from './sections/About';
-import Contact from './sections/Contact';
-import Header from './sections/Header';
-import Projects from './sections/Projects';
-import Skills from './sections/Skills';
-import Navbar from './sections/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
 
 const App = () => {
-  const [preloader, setPreload] = useState(true);
-  const [timer, setTimer] = useState(3);
-
-  const id = useRef(null);
-
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreload(false);
-  };
-
-  useEffect(() => {
-    id.current = window.setInterval(() => {
-      setTimer((time) => time - 1);
-    }, 1000);
-    return () => clear();
-  }, []);
-
-  useEffect(() => {
-    if (timer === 0) {
-      clear();
-    }
-  }, [timer]);
-
-  if (preloader) {
-    return (
-      <div className='loader-wrapper absolute'>
-        <h1>mathis humbert</h1>
-        <h2>portfolio</h2>
-      </div>
-    );
-  }
-
   return (
-    <>
-      <Navbar />
-      <Header />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/project/:id' element={<ProjectPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,54 @@
+import { useState, useRef, useEffect } from 'react';
+import About from '../sections/About';
+import Contact from '../sections/Contact';
+import Header from '../sections/Header';
+import Projects from '../sections/Projects';
+import Skills from '../sections/Skills';
+import Navbar from '../sections/Navbar';
+
+const HomePage = () => {
+  const [preloader, setPreload] = useState(true);
+  const [timer, setTimer] = useState(3);
+
+  const id = useRef(null);
+
+  const clear = () => {
+    window.clearInterval(id.current);
+    setPreload(false);
+  };
+
+  useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((time) => time - 1);
+    }, 1000);
+    return () => clear();
+  }, []);
+
+  useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer]);
+
+  if (preloader) {
+    return (
+      <div className='loader-wrapper absolute'>
+        <h1>mathis humbert</h1>
+        <h2>portfolio</h2>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
+  );
+};
+
+export default HomePage;
