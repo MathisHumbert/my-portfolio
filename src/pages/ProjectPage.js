383922a6cd7443143274aa940cc7fd57e1a71bf0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-25 10:38:52 +0100

    fixed bug

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import data from '../utils/projectData';
 
 const ProjectPage = () => {
   const { id } = useParams();
